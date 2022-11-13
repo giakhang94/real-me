05f3b6ffc6474f8adbeafa29ce35200e9e2d168e
DESKTOP-3R5KC0E\admin <ngk.khang94@gmail.com>
2022-11-13 14:43:16 +0700

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './App.css';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Profile from './pages/Profile';
+import Office from './pages/Office';
+import SignIn from './pages/SignIn';
 
 function App() {
-  return <div className="App">hello world</div>;
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/forgotpassword" element={<ForgotPassword />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/office" element={<Office />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
